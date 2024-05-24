3aa9760443c69b3035b0334065065953a410850d
Pradeepmathi <pradeepmathi1656@gmail.com>
2024-05-24 15:51:22 +0530

    add style & some function

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,14 +1,26 @@
-import {useState} from 'react'
+import '../Stylsheet/Display.css'
+import {useEffect, useState} from 'react'
 export default function Display(){
-    const [currentTime,setCurrentTime] = useState(new Date())
+    const [currentTime,setCurrentTime] = useState(new Date());
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            setCurrentTime(new Date())
+        },1000);
+        return ()=> clearInterval(timer)
+    },[])
+    function formatTimeWithLeadingZero(){
+        
+    }
+
+    function formatHour (hour){
+        return hour ===0 ? 12 : hour > 12 ? hour -12 : hour ;
+    };
     return(
         <>
         <div className='digital-clock'>
            <h1>digital clock</h1>
-           <div className='time'>06:15:59 PM</div>
-           <div className='date'>Tuesday, february 6, 2024</div>
-
-
+           <div className='time'>{formatHour(currentTime.getHours())}:15:59 PM</div>
+           <div className='date'>Tuesday, February 6, 2024</div>
         </div>
         </>
     )
